99981d1eec96eeb4baeb12ba46f26b2c8751d105
Pharmamara <140740925+Pharmamara@users.noreply.github.com>
2024-09-26 20:16:44 +0300

    w28_form add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+//Компонент имеет два состояния: inputValue хранит введённый текст, formattedText хранит форматированный текст
+const TextFormatter = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [formattedText, setFormattedText] = useState("");
 
-function App() {
+  // Функция обработки ввода. Когда пользователь вводит текст в поле, этот текст сохраняется в inputValue
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  // Функция обработки нажатия кнопки. При нажатии на кнопку вызывается функция handleButtonClick, которая преобразует текст в верхний регистр и сохраняет его в formattedText
+  const handleButtonClick = () => {
+    const formatted = inputValue.toUpperCase();
+    setFormattedText(formatted);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Введите текст"
+      />
+      <button onClick={handleButtonClick}>Показать текст</button>
+
+      {/* Отображаем форматированный текст и стилизуем его красным жирным шрифтом */}
+      {formattedText && (
+        <p style={{ color: "red", fontWeight: "bold" }}>{formattedText}</p>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default TextFormatter;
